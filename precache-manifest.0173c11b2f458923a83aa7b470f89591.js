d3b86873b087599b84236683811ba0f8dae98cc5
kalczur <kalczur@gmail.com>
2020-09-17 17:49:24 +0200

    Updates

---
+++ precache-manifest.0173c11b2f458923a83aa7b470f89591.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79878cfb9234cb2fd38000dc8eb8cb83",
+    "url": "/zycieNaBombiePortal/index.html"
+  },
+  {
+    "revision": "75e5318f330d043180ca",
+    "url": "/zycieNaBombiePortal/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "a1921cc6b3d62fe02ab7",
+    "url": "/zycieNaBombiePortal/static/css/main.85acb93a.chunk.css"
+  },
+  {
+    "revision": "75e5318f330d043180ca",
+    "url": "/zycieNaBombiePortal/static/js/2.906f3ec1.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/zycieNaBombiePortal/static/js/2.906f3ec1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1921cc6b3d62fe02ab7",
+    "url": "/zycieNaBombiePortal/static/js/main.16e2b3bf.chunk.js"
+  },
+  {
+    "revision": "affedfd1bd21a73564d4",
+    "url": "/zycieNaBombiePortal/static/js/runtime-main.9e4aeec8.js"
+  },
+  {
+    "revision": "7476afc0b31e7c6600e0ed510dad33d0",
+    "url": "/zycieNaBombiePortal/static/media/Tapeta_01.7476afc0.jpg"
+  },
+  {
+    "revision": "0fc6bf6d2c1c9d28754209a7bd002c89",
+    "url": "/zycieNaBombiePortal/static/media/doctor.0fc6bf6d.png"
+  },
+  {
+    "revision": "b89b5217d1686a19a73d751501c8359a",
+    "url": "/zycieNaBombiePortal/static/media/ibrahim-boran-zsKFQs2kDpM-unsplash.b89b5217.jpg"
+  },
+  {
+    "revision": "9db8fbd700269eb24034179896672b58",
+    "url": "/zycieNaBombiePortal/static/media/pexels-karolina-grabowska-4047148.9db8fbd7.jpg"
+  },
+  {
+    "revision": "87263c93a58d3870d17cac0660aaf19a",
+    "url": "/zycieNaBombiePortal/static/media/pexels-victor-freitas-949126.87263c93.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/zycieNaBombiePortal/precache-manifest.0173c11b2f458923a83aa7b470f89591.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zycieNaBombiePortal/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
